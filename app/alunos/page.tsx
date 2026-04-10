024d1ef0bed724cf6af4be4746486dc793f5131f
SamuelMucham <samuelmucham0607@gmail.com>
2026-04-10 11:47:27 -0300

    Add files via upload

---
+++ app/alunos/page.tsx
@@ -0,0 +1,23 @@
+export default function AlunosPag(){
+    return(
+        <div className="justify-center flex flex-col gap-100">
+            <h1 className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black text-4xl font-bold ">Lista de alunos</h1>
+
+            <div className="flex flex-col flex-1 items-center justify-center ">
+                <ul className=" justify-center flex flex-col gap-10 ">
+                    <li className="  bg-indigo-500 opacity-50 flex h-12 w-full items-center justify-center gap-5  bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] outline-2 outline-offset-2 outline-pink-500">Aluno 1</li>
+                    <li className=" bg-indigo-500 opacity-50 flex h-12 w-full items-center justify-center gap-5  bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] outline-2 outline-offset-2 outline-pink-500">Aluno 2</li>
+                    <li className="bg-indigo-500 opacity-50 flex h-12 w-full items-center justify-center gap-5 bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] outline-2 outline-offset-2 outline-pink-500">Aluno 3</li>
+                    <li className="bg-indigo-500 opacity-50 flex h-12 w-full items-center justify-center gap-5  bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] outline-2 outline-offset-2 outline-pink-500">Aluno 4</li>
+                    <li className=" bg-indigo-500 opacity-50 flex h-12 w-full items-center justify-center gap-5  bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] outline-2 outline-offset-2 outline-pink-500">Aluno 5</li>
+                    <li className="bg-indigo-500 opacity-50 flex h-12 w-full items-center justify-center gap-5 bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] outline-2 outline-offset-2 outline-pink-500">Aluno 6</li>
+                    <li className="bg-indigo-500 opacity-50 flex h-12 w-full items-center justify-center gap-5  bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] outline-2 outline-offset-2 outline-pink-500">Aluno 7</li>
+                    <li className=" bg-indigo-500 opacity-50 flex h-12 w-full items-center justify-center gap-5  bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] outline-2 outline-offset-2 outline-pink-500">Aluno 8</li>
+                    <li className="bg-indigo-500 opacity-50 flex h-12 w-full items-center justify-center gap-5 bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] outline-2 outline-offset-2 outline-pink-500">Aluno 9</li>
+                    <li className="bg-indigo-500 opacity-50 flex h-12 w-full items-center justify-center gap-5 bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px] outline-2 outline-offset-2 outline-pink-500">Aluno 10</li>
+                </ul>
+
+            </div>
+        </div>
+    )
+}
